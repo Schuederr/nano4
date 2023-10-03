93984ac1724ccfc629a5710868351d9c684bc1d0
Natalia Schueda <nati.160599@gmail.com>
2023-10-03 10:39:34 -0300

    Initial Commit

---
+++ nano4/nano4App.swift
@@ -0,0 +1,17 @@
+//
+//  nano4App.swift
+//  nano4
+//
+//  Created by Natalia Schueda on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct nano4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
